ef606ed1666185fbf6097386765f91f7bf58c11f
realz <real_tencrew@hotmail.com>
2017-04-23 20:59:12 +0700

    s

---
+++ app/src/main/java/com/example/realz/sasapplication/UserActivity.java
@@ -0,0 +1,25 @@
+package com.example.realz.sasapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class UserActivity extends AppCompatActivity {
+
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+
+        textView = (TextView) findViewById(R.id.textlogin);
+
+        Intent intent = getIntent();
+
+        String username = intent.getStringExtra(MainActivity.USER_NAME);
+
+        textView.setText("Welcome User "+username);
+    }
+
+}
